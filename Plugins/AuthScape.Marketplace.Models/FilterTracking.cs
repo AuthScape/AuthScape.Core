5da7a61689548143009842ecf445153f027b7f4d
Brandon Zuech <Brandonzuech@hotmail.com>
2025-02-14 18:49:02 -0500

    small progress

--- Plugins/AuthScape.Marketplace.Models/FilterTracking.cs
+++ Plugins/AuthScape.Marketplace.Models/FilterTracking.cs
@@ -3,6 +3,7 @@
     public class FilterTracking
     {
         public string Category { get; set; }
+        public string Subcategory { get; set; }
         public string Option { get; set; }
     }
 }
