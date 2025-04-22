6daf7d39d824d058de8da282fc2f988749e03b94
Brandon Zuech <Brandonzuech@hotmail.com>
2025-04-21 22:51:00 -0400

    location custom fields

---
+++ Plugins/AuthScape.UserManagementSystem.Models/LocationCustomField.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthScape.UserManagementSystem.Models
+{
+    public class LocationCustomField
+    {
+        public long LocationId { get; set; }
+        public Guid CustomFieldId { get; set; }
+        public string Value { get; set; }
+
+        public CustomField CustomField { get; set; }
+    }
+}
---
+++ Services.Database/Migrations/20250422020215_RemoveCompanyIdFromCustomField.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Services.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCompanyIdFromCustomField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LocationCustomFields",
+                table: "LocationCustomFields");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "LocationCustomFields");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LocationCustomFields",
+                table: "LocationCustomFields",
+                columns: new[] { "LocationId", "CustomFieldId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LocationCustomFields",
+                table: "LocationCustomFields");
+
+            migrationBuilder.AddColumn<long>(
+                name: "CompanyId",
+                table: "LocationCustomFields",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LocationCustomFields",
+                table: "LocationCustomFields",
+                columns: new[] { "LocationId", "CompanyId", "CustomFieldId" });
+        }
+    }
+}
