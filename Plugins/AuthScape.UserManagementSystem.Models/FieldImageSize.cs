b9e60ffbfb062f9e2e7e9f319dee368dc794c5f5
Brandon Zuech <Brandonzuech@hotmail.com>
2025-05-23 18:46:00 -0400

    file image sizes

---
+++ Plugins/AuthScape.UserManagementSystem.Models/FieldImageSize.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthScape.UserManageSystem.Models
+{
+    public class FieldImageSize
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
