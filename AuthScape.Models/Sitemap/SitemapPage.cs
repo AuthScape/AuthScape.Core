566329ea57e25c127c191d07e0236dd5889c8cf4
Brandon Zuech <Brandonzuech@hotmail.com>
2025-10-14 19:06:38 -0400

    working on sitemap

---
+++ AuthScape.Models/Sitemap/SitemapPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AuthScape.Models.Sitemap
+{
+    public class SitemapResponse
+    {
+        public List<SitemapPage> Pages { get; set; }
+    }
+
+    public class SitemapPage
+    {
+        [JsonPropertyName("slug")]
+        public string Slug { get; set; }
+
+        [JsonPropertyName("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonPropertyName("changefreq")]
+        public string Changefreq { get; set; } = "weekly"; // daily, weekly, monthly, yearly
+
+        [JsonPropertyName("priority")]
+        public string Priority { get; set; } = "0.7"; // 0.0 to 1.0
+    }
+}
---
+++ Plugins/AuthScape.Sitemap/SitemapService/SitemapService.cs
@@ -0,0 +1,25 @@
+﻿using AuthScape.Models.Sitemap;
+using System.Web.Mvc;
+
+namespace Authscape.Sitemap
+{
+    public class SitemapService
+    {
+        [HttpGet("v1/pages")]
+        public async Task<IActionResult<SitemapResponse>> GetSitemapPages()
+        {
+            var pages = await _context.Pages
+                .Where(p => p.IsPublished)
+                .Select(p => new SitemapPage
+                {
+                    Slug = p.Slug,
+                    UpdatedAt = p.UpdatedAt,
+                    Changefreq = p.Changefreq ?? "weekly",
+                    Priority = p.Priority ?? "0.7"
+                })
+                .ToListAsync();
+
+            return new SitemapResponse { Pages = pages };
+        }
+    }
+}
